c8ca46448fcf323c89e7ac9d534ef5ea372f5ccd
Myota <allister.wylie@mohawkcollege.ca>
2026-03-18 15:19:20 -0400

    Create light following script

---
+++ mimicstheatre/Assets/Scripts/Follower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public GameObject lightPoint;
+    public float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0, 0, 0);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lpTrack = new Vector3(lightPoint.transform.position.x, 0, lightPoint.transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, lpTrack, speed * Time.deltaTime);
+    }
+}
