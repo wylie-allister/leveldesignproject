062fd21dd5cd85f0c617338255ce82104fc10442
Allister Wylie <allister.wylie@mohawkcollege.ca>
2026-03-19 17:58:55 -0400

    Add rudimentary enemy detection

--- mimicstheatre/Assets/Scripts/Enemy.cs
+++ mimicstheatre/Assets/Scripts/Enemy.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-
-    
+    public LayerMask player;
+    public LayerMask ob;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Rotation();
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (!Physics.Raycast(transform.position, fwd, 2, ob))
+            {
+            if (Physics.Raycast(transform.position, fwd, 5, player))
+            {
+                Debug.Log("AAAAAAAAAA");
+                UhOh();
+            }
+        }
+    }
+
+    public void UhOh()
+    {
+        //replace with waypoints oh my god do not leave this code like this
+        SceneManager.LoadScene("Theatre");
     }
 
     public void Rotation()
